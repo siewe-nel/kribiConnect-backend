4933feba9bba3fc2bd255b1d7e9c933abee41591
siewe-nel <ngatchnel1@gmail.com>
2025-11-23 17:20:42 +0100

    including mysql2

--- config/db.js
+++ config/db.js
@@ -4,10 +4,11 @@ const { Sequelize } = require('sequelize');
 // CREATE DATABASE kribiconnect_db;
 
 console.log('--- [Config] Initialisation de la connexion Sequelize...');
-
+const mysql2 = require('mysql2');
 const sequelize = new Sequelize('sql3809084', 'sql3809084', 'ZCpGNsMQhX', {
   host: 'sql3.freesqldatabase.com',
   dialect: 'mysql',
+  dialectModule: mysql2,
   logging: false, // Mettre à 'true' pour voir les requêtes SQL brutes dans la console
 });
 
